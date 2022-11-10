24f5832d58d4f8565356d8b40ee0c8383326b63c
jessica <jmillene12@gmail.com>
2022-11-10 14:10:27 -0300

    requisito 14

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,7 +1,9 @@
 db.produtos.updateMany(
   { nome: { $ne: "McChicken" } },
-  { $addToSet: {
-    ingredientes: "ketchup",
-  } },
+  {
+    $addToSet: {
+      ingredientes: "ketchup",
+    },
+  },
 );
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
